d34c420e634b3bf7fa7f80f3e979f2ca6549a968
ChillZilla <Amanda.akin@live.com>
2016-04-28 19:20:08 -0500

    Added files via upload
    changed  a lot

--- Driver.java
+++ Driver.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 
@@ -8,37 +10,52 @@ public class Driver {
 		Mastermind game = new Mastermind();
 		int moves =0;
 		boolean play = false;
-		int result = JOptionPane.showConfirmDialog(null, 
-				   "Are you ready to play some Mastermind???",null, JOptionPane.YES_NO_OPTION);
-				if(result == JOptionPane.YES_OPTION) {
-				    play = true;
-				    moves =0;
-				} 
+		//move everything to console
+		//add options to change the board configurations
+		
+		boolean ready = game.readyToStart();
+		if(ready){
+		play = true;
+		moves = 0;
+		game.customize();
+		game.generateBoard();
+			
 		while(play)
 		{
-			game.generateBoard();
-			
-			
 			String move = game.getMove();
 			moves ++;
 			
 			//call the move output function : checks how off they are from the input
 			boolean didWin = game.moveChecker(move); //check the current move to see if it matches the right answer
-			if(didWin)			
+			if(didWin){			
 				play = game.endGame(true);
-			
-			else if(moves == 12)			
+			game.generateBoard();
+			}
+			else if(moves == 12){			
 				play = game.endGame(false);
-			
+			game.generateBoard();
+			}
 			else //the game is not over so we need to generate the pegs
 			{
 				//JOptionPane.showMessageDialog(null, "Your guess: " + move);
 				String pegs = game.pegsMove(move);
 				String[] pegBW = pegs.split("\\s+"); //split by space
-				JOptionPane.showMessageDialog(null, "Your guess: " + move + " Black Pegs: " + pegBW[0] + " White Pegs: " + pegBW[1]);
+				System.out.println("Your guess: " + move + " Black Pegs: " + pegBW[0] + " White Pegs: " + pegBW[1]);
 				
 				
 			}
+			if(moves == 12 && play == true)
+			{
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				game.customize();
+				System.out.println("Generating secret code...");
+				game.generateBoard();
+				
+			}
 		}
-	}
+		}
+		
+		System.out.println("Closing MasterMind...Thanks for playing");
+		}
+	
 }
--- Mastermind.java
+++ Mastermind.java
@@ -1,6 +1,7 @@
 import java.awt.Component;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -11,34 +12,214 @@ public class Mastermind {
 	 * History output:
 	 * "guess" + "peg output"
 	 * */
-int  numMoves;
 private Random random = new Random();
 private ArrayList<ArrayList<String>> history;
 private ArrayList<Character> secretCode;
 private static int attemptsRemain;
+private static int numColors;
+private static int numPegs;
+private static int numGuesses;
 private final char  orange = 'O'; 
 private final char blue = 'B';
 private final char purple = 'P';
 private final char red = 'R';
 private final char yellow = 'Y';
 private final char green = 'G';
+private ArrayList<Character> Colors;
 private final String empty = "Z";
 
+
+public boolean readyToStart()
+{
+	this.Colors = new ArrayList<Character>();
+	this.numColors = 6;
+	this.numGuesses = 12;
+	this.numPegs = 4; 
+	addColors();
+	System.out.println("Welcome to Mastermind. Here are the rules.\n");
+	System.out.println("This is a text version of the classic board game Mastermind.\n"
+			+ "The computer will think of a secret code. The code consists of 4 colored pegs.\n" 
+			+ "The pegs may be one of six colors: blue, green, orange, purple, red, or yellow.\n"
+					+ "A color may appear more than once in the code. You try to guess what colored pegs are in the code and what order they are in.\n"
+					+ "After you make a valid guess the result (feedback) will be displayed.\n"
+					+ "The result consists of a black peg for each peg you have guessed exactly correct (color and position) in your guess.\n"
+					+ "For each peg in the guess that is the correct color, but is out of position, you get a white peg.\n"
+					+ "For each peg, which is fully incorrect, you get no feedback.\n "
+					+ "Only the first letter of the color is displayed. B for Blue, R for Red, and so forth.\n"
+					+ "When entering guesses you only need to enter the first character of each color as a capital letter.\n "
+					+ "You have 12 guesses to figure out the secret code or you lose the game. Are you ready to play? (Y/N): ");
+	Scanner input = new Scanner(System.in);
+	String answer = input.nextLine();
+	boolean read = false;
+	boolean wantToPlay = false;
+	while(!read)
+	{
+		
+		if(answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("Y"))
+		{
+		
+			System.out.println("Yay! Let's begin...");
+			read = true;
+			wantToPlay = true;
+			
+		}
+		else if(answer.equalsIgnoreCase("No")||answer.equalsIgnoreCase("N"))
+		{
+			
+			System.out.println("That's too bad...See you next time!");
+			read = true;
+		}
+		else
+		{
+			
+			System.out.println("Please answer Y or N\n");
+			System.out.println("Are you ready to play? (Y/N): ");
+			answer = input.nextLine();
+		}
+		
+	}
+	return wantToPlay;
+}
+
+public void customize()
+{
+	System.out.println("~~~~~~~~~~~~");
+	System.out.println("Main Menu:");
+	System.out.println("Start");
+	System.out.println("Settings");
+	System.out.println("~~~~~~~~~~~~");
+	System.out.println("Type 'Start' if you are ready. Type 'Settings' to customize your game.\n");
+	
+	Scanner input = new Scanner(System.in);
+	String answer = input.nextLine();
+	boolean read = false;
+	if(answer.equalsIgnoreCase("Start"))
+	{
+		System.out.println("Settings: " + this.numColors + " Colors " + this.numPegs + " Pegs " + this.numGuesses + " Guesses" );
+		return;
+	}
+	while(!read)
+	{
+		if(answer.equalsIgnoreCase("Play"))
+		{
+			System.out.println("Settings: " + this.numColors + " Colors " + this.numPegs + " Pegs " + this.numGuesses + " Guesses" );
+			read = true;
+		}
+		else if(answer.equalsIgnoreCase("Settings"))
+		{
+			System.out.println("Welcome to Settings");
+			System.out.println("------------------------");
+			System.out.println("Type what you would like to modify: Colors, Pegs, Guesses?" + "\n" + "Type 'Done' to return");
+			answer = input.nextLine();
+			while(!answer.equalsIgnoreCase("Done"))
+			{
+					if (answer.equalsIgnoreCase("Colors")) {
+						System.out.println("Please enter the color you would like to add:");
+						answer = input.nextLine();
+						while (!answer.equalsIgnoreCase("Exit")) {
+							Character firstletter = answer.substring(0, 1).charAt(0);
+							firstletter.toUpperCase(firstletter);
+							
+							boolean doesExist = letterExists(firstletter);
+
+							if (doesExist) {
+								System.out.println("Please use a color that doesn't start with " + firstletter + ".");
+							} else {
+								Colors.add(firstletter); // add the first letter of the color to the string.
+								System.out.println(
+										"Added your color. Please use the first letter of your color to reference it in the game.");
+								System.out.println(
+										"To keep adding colors, type the colors name you wish to add. Type 'Exit' to exit Color Settings.");
+
+							}
+							answer = input.nextLine();
+						}
+					
+				}
+				else if(answer.equalsIgnoreCase("Pegs"))
+				{
+					System.out.println("Choose the number of Pegs you want: 4, 6, 8. Or type Exit to go back to Settings.");
+					answer = input.nextLine();
+					boolean pickedPeg = false;
+					while(!pickedPeg){
+					if(answer.equalsIgnoreCase("4")|| answer.equals("6")|| answer.equals("8"))
+					{
+						System.out.println("You chose:" + " " + answer + " Pegs");
+						this.numPegs = Integer.parseInt(answer);
+						pickedPeg = true;
+					}
+					else
+					{
+						System.out.println("Please input only 4, 6, or 8:");
+						answer = input.nextLine();
+					}
+					
+					}
+					
+				}
+				else if(answer.equalsIgnoreCase("Guesses"))
+				{
+					System.out.println("How many guesses would you like? (Pick any number from 1 to 99)");
+					answer = input.nextLine();
+						boolean guessSet = false;
+						while (!guessSet) {
+							if (answer.matches("\\d+\\d") || answer.matches("\\d")) {
+
+								this.numGuesses = Integer.parseInt(answer);
+								guessSet = true;
+								System.out.println("Your guess number has increased to: " + this.numGuesses);
+							} else {
+								System.out.println("Please enter a number from 1 to 99");
+							
+								answer = input.nextLine();
+							}
+						}
+				}
+				else
+				{
+					System.out.println("Error: Illegal Settings Command.");
+				
+				}
+				System.out.println("--------------------------------");
+				System.out.println("Type what you would like to modify: Colors, Pegs, Guesses?" + "\n" + "Type 'Done' to return");
+				answer = input.nextLine();
+			}
+			
+		}
+		else
+		{
+			System.out.println("Please type 'Play' or 'Settings'");
+			answer = input.nextLine();
+		}
+		
+		
+		
+	}
+
+}
+
+public boolean letterExists(Character letter)
+{
+	
+	for(int i = 0; i < Colors.size(); i ++)
+	{
+		if(Colors.get(i).equals(letter))
+		{
+			return true;
+		}
+	}
+	return false;
+}
 /* *
  * fill the various randomly generated pieces of the game as well as initializes variables
  * for the player
  * */
 public void generateBoard()
-{	this.numMoves =0;
+{	
 	this.secretCode = new ArrayList<Character>();
 	this.history = new ArrayList<ArrayList<String>>(); 
-	attemptsRemain = 12;
-	for(int i =0; i < 12; i++)
-	{
-		ArrayList<String> toAdd = new ArrayList<String>();
-		history.add(toAdd);
-	}
-	for(int i =0; i < 4; i++)
+	attemptsRemain = this.numGuesses;
+	/*for(int i =0; i < 4; i++)
 	{
 		int color = random.nextInt()%5;
 		switch(color)
@@ -63,10 +244,26 @@ public void generateBoard()
 			break;
 		default:
 			secretCode.add(this.green);
+		}*/
+		for(int i = 0; i < this.numPegs; i ++)
+		{
+			int color = (int) (Math.random()*Colors.size());
+			this.secretCode.add(Colors.get(color));
 		}
-		
 	}
+
+
+public void addColors()
+{
+	Colors.add('O');
+	Colors.add('P');
+	Colors.add('B');
+	Colors.add('Y');
+	Colors.add('G');
+	Colors.add('R');
+	
 }
+
 public void generateHistory()
 {
 	for(int i =0; i < history.size(); i++)
@@ -87,35 +284,27 @@ String ret = "";
 boolean valid = false;
 
 while (!valid){
-String move = JOptionPane.showInputDialog(frame, message + "What is your move?");
-move = move.toUpperCase();
-if(move.equals("HISTORY"))
-{
-	ret = move;
-	valid = true;
-}
-else if(confirmMove(move) == true)
+System.out.println(message + " Please enter your guess: ");
+Scanner input = new Scanner(System.in);
+String move = input.nextLine();
+if(confirmMove(move) == true)
 {
 ret = move.toUpperCase();
 valid = true;
 }
 }
-if(!ret.equals("HISTORY")){
-history.get(numMoves).add(ret);
 attemptsRemain--;
-}
 return ret;
 }
-
 
 private boolean confirmMove(String move) {
 	move = move.toUpperCase();
 	if(move.equals("HISTORY"))
 		return true;
-	else if(move.length() != 4)
+	else if(move.length() != this.numPegs)
 		return false;
 	else{
-		for(int i =0; i < 4; i++)
+		for(int i =0; i < this.numPegs; i++)
 		{
 			char test = move.charAt(i);
 			switch(test)
@@ -133,6 +322,30 @@ private boolean confirmMove(String move) {
 	}
 	return true;
 }
+public static int getNumColors() {
+	return numColors;
+}
+
+public static void setNumColors(int numColors) {
+	Mastermind.numColors = numColors;
+}
+
+public static int getNumPegs() {
+	return numPegs;
+}
+
+public static void setNumPegs(int numPegs) {
+	Mastermind.numPegs = numPegs;
+}
+
+public static int getNumGuesses() {
+	return numGuesses;
+}
+
+public static void setNumGuesses(int numGuesses) {
+	Mastermind.numGuesses = numGuesses;
+}
+
 /* *
  * i think this is redundant because we implement this basic sequence in driver
  * */
@@ -195,44 +408,54 @@ public String pegsMove(String move)
 		}
 	}
 	}
-	String histAdd = bPegs + " black pegs and " + wPegs + " white pegs";
-	history.get(numMoves).add(histAdd);
-	numMoves++;
 	String ret = Integer.toString(bPegs) + " " + Integer.toString(wPegs);
 	return ret;
 	}
 public boolean endGame(boolean win)
 {
 	boolean ret = false;
-	Object[] options = {"OK"};
-	JFrame frame = new JFrame();	
+	//Object[] options = {"OK"};
+	//JFrame frame = new JFrame();	
 
 	if(win)
 	{
-	    int n = JOptionPane.showOptionDialog(frame,
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~~~You win!!~~~~~~~");
+	  /*  int n = JOptionPane.showOptionDialog(frame,
                 "You win!! ","Dank Games Inc.",
                 JOptionPane.PLAIN_MESSAGE,
                 JOptionPane.QUESTION_MESSAGE,
                 null,
                 options,
-                options[0]);
+                options[0]);*/
 	}
 	else{
-	    int n = JOptionPane.showOptionDialog(frame,
+		System.out.println("You lost :( \n");
+		System.out.println("The secret code was: " + this.secretCode); 
+	    /*int n = JOptionPane.showOptionDialog(frame,
                 "You lost :( ","Dank Games Inc.",
                 JOptionPane.PLAIN_MESSAGE,
                 JOptionPane.QUESTION_MESSAGE,
                 null,
                 options,
-                options[0]);
+                options[0]);*/
 		
 	}
-	int result = JOptionPane.showConfirmDialog(null, 
+	/*int result = JOptionPane.showConfirmDialog(null, 
 			   "Great game, let's play another?",null, JOptionPane.YES_NO_OPTION);
 			if(result == JOptionPane.YES_OPTION) {
 			    ret = true;
+			}*/
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	System.out.println("Hit any key to play again. Hit 'Q' to quit.");
+	Scanner input = new Scanner(System.in);
+	String playAgain = input.nextLine();
+			if(!playAgain.equalsIgnoreCase("Q"))
+			{
+				ret = true;
+			
 			}
-			return ret;
+		return ret;
 }
 
 }//class
